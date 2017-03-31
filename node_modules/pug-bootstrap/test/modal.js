e311814c168c64ad79196792358d50761465182f
abhilekh singh <abhilekhsingh041992@gmail.com>
2017-03-31 17:22:17 +0400

    Cleaned up

--- node_modules/pug-bootstrap/test/modal.js
+++
@@ -1,9 +0,0 @@
-var pug = require("pug");
-var assert = require("assert");
-var path = require("path");
-
-describe("Modal", function() {
-    it("should generate a modal",function() {
-        var fn = pug.compileFile(path.join(__dirname,"fixtures/modal","modal.pug"));
-    });
-});
